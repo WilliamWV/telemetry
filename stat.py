5917e88850f9791dae6a0777e83d03c7b8cfdb8d
wwvargas <william.w.vargas@gmail.com>
2019-06-17 01:11:54 +0000

    General fixes on statistical controller

--- stat.py
+++ stat.py
@@ -20,7 +20,7 @@ RULES_DIR = 'rules'
 # minimum time between two congestion reports from the same switch in seconds
 CONGESTION_TIME = 1 
 # thresholds from which a switch will be considered congested
-DELAY_THRESHOLD = 1000 # ns
+DELAY_THRESHOLD = 0 # us
 QUEUE_THRESHOLD = 30 # packets on queue
 
 class Rule:
@@ -33,7 +33,7 @@ class Rule:
     self.egress_port = port
     self.times_used = 0
 
-  def increment_uses():
+  def increment_uses(self):
     self.times_used += 1
 
 
@@ -49,6 +49,7 @@ class Flow:
   def increment_pkts(self):
     self.num_of_pkts += 1
     self.last_use = time.time()
+    self.active = True
 
   def verify_active(self):
     if (time.time() - self.last_use > ACTIVE_THRESHOLD):
@@ -61,14 +62,15 @@ class Switch:
     self.name = name
     self.avg_delay = 0
     self.avg_queue_ocupacy = 0
+    self.pkts = 0
     self.flows = {} # {flow source -> Flow class instance}
     self.rules = {} # {rule id     -> Rule class instance}
     self.last_congestion_print = -1
-    self.init_rules():
+    self.init_rules()
 
   def init_rules(self):
     global RULES_DIR
-    rules_file = open(RULES_DIR + self.name)
+    rules_file = open(RULES_DIR + '/' + self.name)
     for line in rules_file.readlines():
       r = ast.literal_eval(line)
       rule = Rule(r['id'], r['match_field'][0], r['match_field'][1], r['port'])
@@ -88,34 +90,38 @@ class Switch:
     try:
       flow = self.flows[pkt[IP].src]
       flow.increment_pkts()
-      rule = self.rules[pkt[SwitchTrace].rule_id]
-      rule.increment_uses()
-      if (pkt[SwitchTrace].qdepth > QUEUE_THRESHOLD or pkt[SwitchTrace].timedelta) and time.time() - self.last_congestion_print > CONGESTION_TIME:
-        print_congestion()
-        self.last_congestion_print = time.time()
     except KeyError:
       new_flow = Flow(time.time(), pkt[IP].src)
       new_flow.increment_pkts()
-      self.add_flow()
+      self.add_flow(new_flow)
 
+    rule = self.rules[pkt[SwitchTrace].rule_id]
+    rule.increment_uses()
+    if (pkt[SwitchTrace].qdepth > QUEUE_THRESHOLD or pkt[SwitchTrace].timedelta > DELAY_THRESHOLD) and time.time() - self.last_congestion_print > CONGESTION_TIME:
+      self.print_congestion()
+      self.last_congestion_print = time.time()
+
+    self.avg_delay = ((self.avg_delay * self.pkts) + pkt[SwitchTrace].timedelta) / (self.pkts + 1)  
+    self.avg_queue_ocupacy = ((self.avg_queue_ocupacy * self.pkts) + pkt[SwitchTrace].qdepth) / (self.pkts + 1)
+    self.pkts += 1
   
   def verify_flows(self):
-    active_flows = [f for f in flows.values() if f.active]
+    active_flows = [f for f in self.flows.values() if f.active]
     for f in active_flows:
       f.verify_active()
 
   
   def print_congestion(self):
-    print '===================  CONGESTION REPORT  ==================='
+    print '========================  CONGESTION REPORT  ========================'
     print 'Congestion on switch ' + str(self.name) + ' caused by the following flows'
-    active_flows = [f for f in flows.values() if f.active]
+    active_flows = [f for f in self.flows.values() if f.active]
     for f in active_flows:
-      print '\tFlow from ' + str(f.src) + ' started ' + str(time.time() - f.init_time) + ' seconds ago -> ' + str(f.num_of_pkts)
-    print 'This congestion is causing an average delay of ' + str(self.avg_delay) + 'ms'
+      print '\tFlow from ' + str(f.src) + ' started ' + '%.2f' % (time.time() - f.init_time) + ' seconds ago -> ' + str(f.num_of_pkts) + ' packets'
+    print 'This congestion is causing an average delay of ' + str(float(self.avg_delay)/1000.0) + 'ms'
     print 'The queue ocupacy of this switch is ' + str(self.avg_queue_ocupacy) + '%'
     print 'The forwarding rules of this switch are:'
     for rule in self.rules.values():
-      print '\t' + str(rule.id) + ') ' + str(rule.key_addr) + '/' + str(rule.prefix_size) + ' => port ' + str(rule.port) + ' (used ' + str(rule.times_used) + ' times)'
+      print '\tRule ' + str(rule.id) + ') ' + str(rule.key_addr) + '/' + str(rule.prefix_size) + ' => port ' + str(rule.egress_port) + ' (used ' + str(rule.times_used) + ' times)'
 
 
 #maps sources
@@ -161,16 +167,17 @@ prev_time = time.time()
 switchs = {} # switch id -> Switch class instance
 
 def handle_pkt(pkt):
-    
+    global prev_time
     if(time.time() - prev_time > VERIFY_TIME):
       for sw in switchs.values():
         sw.verify_flows()
+      prev_time = time.time()
 
-    switchs[pkt[SwitchTrace].swid].income_pkt(pkt) 
-
-
-
-    print 'timestamp = ' + str(pkt[SwitchTrace].timestamp)
+    try:
+      switchs[pkt[SwitchTrace].swid].income_pkt(pkt) 
+    except KeyError:
+      switchs[pkt[SwitchTrace].swid] = Switch('s' + str(pkt[SwitchTrace].swid))
+      switchs[pkt[SwitchTrace].swid].income_pkt(pkt)
 
     print "got a packet"
     pkt.show2()
